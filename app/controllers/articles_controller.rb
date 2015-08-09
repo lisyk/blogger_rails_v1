ff91f728de49ad61c1947646bffc8d3b0f3991c8
Lisyk <sergey.liskovoy@gmail.com>
2015-08-08 22:04:08 -0400

    add create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,25 @@
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
 
   def new
-    @articles = Article.new
+    @article = Article.new
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
