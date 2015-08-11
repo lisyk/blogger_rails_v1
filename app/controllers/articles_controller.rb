38a4cde5b016a0aceb26a6d6e3f4e0c316be2b41
Lisyk <sergey.liskovoy@gmail.com>
2015-08-11 11:32:18 -0400

    tagging feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,6 +41,6 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :tag_list, :body)
   end
 end
